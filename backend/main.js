ca7be6cf21f5352b402963fa3fa5d82919c75fe1
Ayaan <ayaann05ansari@gmail.com>
2025-11-27 22:27:55 +0530

    Update CORS

--- backend/main.js
+++ backend/main.js
@@ -16,7 +16,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://banking-system-17ik.onrender.com",
+    origin:
+      "https://banking-system-17ik.onrender.com" || "http://localhost:5173",
     credentials: true,
   })
 );
