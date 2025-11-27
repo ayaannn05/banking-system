3a8931b6862b8bda6e5d78cad14e8ad39713dc7a
Ayaan <ayaann05ansari@gmail.com>
2025-11-27 23:16:06 +0530

    Update CORS configuration to use a specific frontend URL

--- backend/main.js
+++ backend/main.js
@@ -16,8 +16,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "https://banking-system-1-zc2l.onrender.com/",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
 
